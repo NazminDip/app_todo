1b15ddd1529f7c3949f16486c85aab8d7b84f005
Nazmin <48346432+NazminDip@users.noreply.github.com>
2022-12-10 02:43:26 +0600

    Update first_page.dart

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -14,13 +14,18 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
-//  bool validgm = RegExp("^[a-z0-9](\.?[a-z0-9]){5,}@gmail\.com").hasMatch(value);
-  final _passwordController = TextEditingController();
-  final _gmailController = TextEditingController();
-  String gmail = "";
-  String password = '';
+  TextEditingController userController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   final formkey = GlobalKey<FormState>();
   var isVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    userController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,23 +58,9 @@ class _FirstPageState extends State<FirstPage> {
                       Padding(
                         padding: const EdgeInsets.all(15),
                         child: TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Enter Your Gmail';
-                            }
-                            bool validgm =
-                                RegExp("^[a-z0-9](\.?[a-z0-9]){5,}@gmail\.com")
-                                    .hasMatch(value);
-                            if (!validgm) {
-                              return "Enter Valid Gmail";
-                            }
-                            return "Wwwwwwww";
-                          },
-                          onChanged: (value) {
-                            setState(() {
-                              gmail = value;
-                            });
-                          },
+                          validator: ((value) {
+                            return emailValidation(value);
+                          }),
                           decoration: const InputDecoration(
                             filled: true,
                             fillColor: Color.fromARGB(255, 236, 231, 231),
@@ -78,9 +69,6 @@ class _FirstPageState extends State<FirstPage> {
                                 borderRadius: BorderRadius.all(
                                   Radius.circular(50),
                                 )),
-                            hintText: 'Enter your Gamail',
-                            hintStyle:
-                                TextStyle(fontSize: 15, color: Colors.red),
                             label: Text('Gmail:'),
                             labelStyle: TextStyle(
                                 fontSize: 20,
@@ -93,20 +81,10 @@ class _FirstPageState extends State<FirstPage> {
                       Padding(
                         padding: const EdgeInsets.all(15),
                         child: TextFormField(
-                          controller: _passwordController,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Enter Password';
-                            } else if (_passwordController.text.length < 6) {
-                              return 'Password at least 6 character';
-                            }
-                            return 'Password';
-                          },
-                          onChanged: (value) {
-                            setState(() {
-                              password = value;
-                            });
-                          },
+                          controller: passwordController,
+                          validator: ((value) {
+                            return passwordValidation(value);
+                          }),
                           obscureText: isVisible ? true : false,
                           decoration: InputDecoration(
                             filled: true,
@@ -115,9 +93,6 @@ class _FirstPageState extends State<FirstPage> {
                                 borderSide: BorderSide.none,
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(50))),
-                            hintText: 'Enter your Password',
-                            hintStyle: const TextStyle(
-                                fontSize: 15, color: Colors.red),
                             label: const Text('Password:'),
                             labelStyle: const TextStyle(
                                 fontSize: 20,
@@ -153,48 +128,8 @@ class _FirstPageState extends State<FirstPage> {
                                 shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(30))),
                             onPressed: () {
-                              final notValid = formkey.currentState!.validate();
-                              _passwordController.clear();
-                              _gmailController.clear();
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text(
-                                        "Your Gmail or Password Is Incorrect",
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 20),
-                                      ),
-                                      backgroundColor:
-                                          Color.fromARGB(255, 240, 128, 165)));
-                              if (notValid) {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const FirstPage()));
-                              } else {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => const ProPage()));
-                              }
+                              logInButton();
                             },
-
-                            // // formkey.currentState!.save();
-                            // final message = 'Username: $gmail';
-                            // // ignore: non_constant_identifier_names
-                            // final snackBar = SnackBar(
-                            //   content: Text(
-                            //     message,
-                            //     style: const TextStyle(fontSize: 15),
-                            //   ),
-                            // );
-                            // ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                            //}
-                            // Navigator.push(
-                            //     context,
-                            //     MaterialPageRoute(
-                            //         builder: (context) => const ProPage()));
-
                             child: const Text(
                               'Login',
                               style: TextStyle(
@@ -239,5 +174,42 @@ class _FirstPageState extends State<FirstPage> {
             )),
           ),
         ));
+  }
+
+  //Validation
+
+  emailValidation(var value) {
+    if (value.isEmpty) {
+      return 'Enter Your Gmail';
+    } else if (!value.contains('@gmail.com')) {
+      return 'Please Enter Valid Gmail';
+    }
+    return null;
+  }
+
+  passwordValidation(value) {
+    if (value.isEmpty) {
+      return 'Enter Your  Password';
+    } else if (value.length <= 7) {
+      return 'Please Password at least 8 Character';
+    }
+    return null;
+  }
+
+  logInButton() {
+    final isValid = formkey.currentState!.validate();
+    if (!isValid) {
+      formkey.currentState!.save();
+      return ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(
+          "Your Gmail or Password Is Incorrect",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        backgroundColor: Colors.pink,
+      ));
+    } else if (isValid) {
+      return Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const ProPage()));
+    }
   }
 }
