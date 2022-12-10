9a606647d14e43988e0b395b8157adf17a3e21fe
Nazmin <48346432+NazminDip@users.noreply.github.com>
2022-12-11 01:11:14 +0600

    Update second_page.dart

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -10,7 +10,17 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  TextEditingController passController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
+  final formkey2 = GlobalKey<FormState>();
   var isVisible = true;
+  @override
+  void initState() {
+    super.initState();
+    passController = TextEditingController();
+    mailController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,38 +44,43 @@ class _SecondPageState extends State<SecondPage> {
                 backgroundImage: AssetImage('images/pro.png'),
               ),
 
-              const Padding(
-                padding: EdgeInsets.all(15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 236, 231, 231),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.all(Radius.circular(50))),
-                    hintText: 'Enter your  Name',
-                    hintStyle: TextStyle(fontSize: 15, color: Colors.red),
-                    label: Text('Full Name:'),
-                    labelStyle: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.pink),
-                    suffix: Icon(Icons.person, color: Colors.pink),
+              Form(
+                key: formkey2,
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: TextFormField(
+                    validator: ((value) {
+                      return mailValidation(value);
+                    }),
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 236, 231, 231),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(50))),
+                      hintText: 'Enter your  Name',
+                      hintStyle: TextStyle(fontSize: 15, color: Colors.red),
+                      label: Text('Full Name:'),
+                      labelStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.pink),
+                      suffix: Icon(Icons.person, color: Colors.pink),
+                    ),
                   ),
                 ),
               ),
 
-              const Padding(
-                padding: EdgeInsets.all(15),
-                child: TextField(
-                  decoration: InputDecoration(
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextFormField(
+                  controller: mailController,
+                  decoration: const InputDecoration(
                     filled: true,
                     fillColor: Color.fromARGB(255, 236, 231, 231),
                     border: OutlineInputBorder(
                         borderSide: BorderSide.none,
                         borderRadius: BorderRadius.all(Radius.circular(50))),
-                    hintText: 'Enter your Gamail',
-                    hintStyle: TextStyle(fontSize: 15, color: Colors.red),
                     label: Text('Gmail:'),
                     labelStyle: TextStyle(
                         fontSize: 20,
@@ -78,15 +93,16 @@ class _SecondPageState extends State<SecondPage> {
 
               Padding(
                 padding: const EdgeInsets.all(15),
-                child: TextField(
+                child: TextFormField(
+                    controller: passController,
+                    validator: ((value) {
+                      return passValidation(value);
+                    }),
                     obscureText: isVisible ? true : false,
                     decoration: InputDecoration(
                       border: const OutlineInputBorder(
                           borderSide: BorderSide.none,
                           borderRadius: BorderRadius.all(Radius.circular(50))),
-                      hintText: 'Enter your Password',
-                      hintStyle:
-                          const TextStyle(fontSize: 15, color: Colors.red),
                       label: const Text('Password:'),
                       labelStyle: const TextStyle(
                           fontSize: 20,
@@ -122,16 +138,7 @@ class _SecondPageState extends State<SecondPage> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(30))),
                     onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content: Text(
-                            "Register is Success, Login Now",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                          backgroundColor: Color.fromARGB(255, 240, 128, 165)));
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const FirstPage()));
+                      regisButton();
                     },
                     child: const Text(
                       'Register',
@@ -148,5 +155,83 @@ class _SecondPageState extends State<SecondPage> {
         ),
       ),
     );
+  }
+
+/////////////////////// Bug
+  // mailValidation(var value) {
+  //   if (value.isEmpty) {
+  //     return 'Enter Valid Gmail';
+  //   } else if (!value.contains('@gmail.com')) {
+  //     return 'Enter Valid Gmail';
+  //   }
+  //   return null;
+  // }
+
+  // passValidation(value) {
+  //   if (value.isEmpty) {
+  //     return 'Enter Password ';
+  //   } else if (value.lenth <= 7) {
+  //     return 'Enter Your Passwordat least 8 Character';
+  //   }
+  //   return null;
+  // }
+
+  // regisButton() {
+  //   final noValid = formkey.currentState!.validate();
+
+  //   if (!noValid) {
+  //     formkey.currentState!.save();
+  //     return ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text(
+  //           "Your Gmail or Password  Format is Incorrect",
+  //           style: TextStyle(color: Colors.white, fontSize: 20),
+  //         ),
+  //         backgroundColor: Color.fromARGB(255, 240, 128, 165)));
+  //   } else {
+  //     Navigator.push(
+  //         context, MaterialPageRoute(builder: (context) => const FirstPage()));
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text(
+  //           "Register is Success, Login Now",
+  //           style: TextStyle(color: Colors.white, fontSize: 20),
+  //         ),
+  //         backgroundColor: Color.fromARGB(255, 240, 128, 165)));
+  //   }
+  // }
+ ////////////////////////////////////////////////////////////////////////////////////Validation
+
+  mailValidation(var value) {
+    if (value.isEmpty) {
+      return 'Enter Your Gmail';
+    } else if (!value.contains('@gmail.com')) {
+      return 'Please Enter Valid Gmail';
+    }
+    return null;
+  }
+
+  passValidation(value) {
+    if (value.isEmpty) {
+      return 'Enter Your  Password';
+    } else if (value.length <= 7) {
+      return 'Please Password at least 8 Character';
+    }
+    return null;
+  }
+
+  regisButton() {
+    final isValid = formkey2.currentState!.validate();
+    if (!isValid) {
+      formkey2.currentState!.save();
+      return ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(
+          "Your Gmail or Password Is Incorrect Format",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        backgroundColor: Colors.pink,
+      ));
+    } else if (isValid) {
+      return Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const FirstPage()));
+    }
   }
 }
